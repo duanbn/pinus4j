cfb7a9f2212b1692796855cc4440216b15502210
duanbn <duanbn@kaolafm.com>
2014-07-29 20:28:59 +0800

    coding jline completor done

--- contrib/pinus-cli/src/main/java/com/pinus/contrib/commandline/App.java
+++ contrib/pinus-cli/src/main/java/com/pinus/contrib/commandline/App.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import jline.Completor;
 import jline.ConsoleReader;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
@@ -49,51 +50,51 @@ public class App {
 	 */
 	private List<DBTable> tables;
 
-    private DBTable _getDBTableBySql(String sql) throws JSQLParserException {
-        // parse table name from sql.
-        Statement st = CCJSqlParserUtil.parse(sql);
-        Select selectStatement = (Select) st;
-        TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
-        List<String> tableNames = tablesNamesFinder.getTableList(selectStatement);
-        if (tableNames.size() != 1) {
-            throw new CommandException("have not support multiple table operation");
-        }
-        String tableName = tableNames.get(0);
+	private DBTable _getDBTableBySql(String sql) throws JSQLParserException {
+		// parse table name from sql.
+		Statement st = CCJSqlParserUtil.parse(sql);
+		Select selectStatement = (Select) st;
+		TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
+		List<String> tableNames = tablesNamesFinder.getTableList(selectStatement);
+		if (tableNames.size() != 1) {
+			throw new CommandException("have not support multiple table operation");
+		}
+		String tableName = tableNames.get(0);
 
-        // find sharding info
-        DBTable dbTable = null;
-        for (DBTable one : this.tables) {
-            if (one.getName().equals(tableName)) {
-                dbTable = one;
-                break;
-            }
-        }
-        if (dbTable == null) {
-            throw new CommandException("cann't find cluster info about table \"" + tableName + "\"");
-        }
+		// find sharding info
+		DBTable dbTable = null;
+		for (DBTable one : this.tables) {
+			if (one.getName().equals(tableName)) {
+				dbTable = one;
+				break;
+			}
+		}
+		if (dbTable == null) {
+			throw new CommandException("cann't find cluster info about table \"" + tableName + "\"");
+		}
 
-        return dbTable;
-    }
+		return dbTable;
+	}
 
-    private SqlNode parseGlobalSqlNode(String cmd) throws CommandException {
-        try {
+	private SqlNode parseGlobalSqlNode(String cmd) throws CommandException {
+		try {
 			String sql = cmd;
 
-            DBTable dbTable = _getDBTableBySql(sql);
-            if (dbTable.getShardingNum() > 0) {
-                throw new CommandException(dbTable.getName() + " is not a global table");
-            }
+			DBTable dbTable = _getDBTableBySql(sql);
+			if (dbTable.getShardingNum() > 0) {
+				throw new CommandException(dbTable.getName() + " is not a global table");
+			}
 
-            //
-            // create sharding key
-            //
+			//
+			// create sharding key
+			//
 			String cluster = dbTable.getCluster();
 
-            DBConnectionInfo connInfo = this.dbCluster.getMasterGlobalConn(cluster);
+			DBConnectionInfo connInfo = this.dbCluster.getMasterGlobalConn(cluster);
 
 			SqlNode sqlNode = new SqlNode();
-            sqlNode.setDs(connInfo.getDatasource());
-            sqlNode.setSql(sql);
+			sqlNode.setDs(connInfo.getDatasource());
+			sqlNode.setSql(sql);
 
 			return sqlNode;
 		} catch (JSQLParserException e) {
@@ -101,7 +102,7 @@ public class App {
 		} catch (DBClusterException e) {
 			throw new CommandException(e.getMessage());
 		}
-    }
+	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private SqlNode parseShardingSqlNode(String cmd) throws CommandException {
@@ -110,35 +111,35 @@ public class App {
 			String sql = cmd.substring(0, cmd.indexOf(KEY_SHARDINGBY) - 1).trim();
 			String shardingValue = cmd.substring(cmd.indexOf(KEY_SHARDINGBY) + 11).trim();
 
-            DBTable dbTable = _getDBTableBySql(sql);
-            if (dbTable.getShardingNum() == 0) {
-                throw new CommandException(dbTable.getName() + " is not a sharding table");
-            }
-            
-            String tableName = dbTable.getName();
-            //
-            // create sharding key
-            //
-			String cluster = dbTable.getCluster();
-            // handle String and Number
-            IShardingKey<?> key = null;
-            if (shardingValue.startsWith("\"") && shardingValue.endsWith("\"")) {
-                key = new ShardingKey<String>(cluster, shardingValue.substring(1, shardingValue.length() - 1));
-            } else {
-                key = new ShardingKey<Long>(cluster, Long.parseLong(shardingValue));
-            }
+			DBTable dbTable = _getDBTableBySql(sql);
+			if (dbTable.getShardingNum() == 0) {
+				throw new CommandException(dbTable.getName() + " is not a sharding table");
+			}
 
-            DB db = null;
-            try {
-                db = this.dbCluster.selectDbFromMaster(tableName, key);
-                System.out.println(db);
-            } catch (DBClusterException e) {
-                throw new RuntimeException(e);
-            }
-            sql = sql.replaceAll(tableName, db.getTableName() + db.getTableIndex());
+			String tableName = dbTable.getName();
+			//
+			// create sharding key
+			//
+			String cluster = dbTable.getCluster();
+			// handle String and Number
+			IShardingKey<?> key = null;
+			if (shardingValue.startsWith("\"") && shardingValue.endsWith("\"")) {
+				key = new ShardingKey<String>(cluster, shardingValue.substring(1, shardingValue.length() - 1));
+			} else {
+				key = new ShardingKey<Long>(cluster, Long.parseLong(shardingValue));
+			}
+
+			DB db = null;
+			try {
+				db = this.dbCluster.selectDbFromMaster(tableName, key);
+				System.out.println(db);
+			} catch (DBClusterException e) {
+				throw new RuntimeException(e);
+			}
+			sql = sql.replaceAll(tableName, db.getTableName() + db.getTableIndex());
 
 			SqlNode sqlNode = new SqlNode();
-            sqlNode.setDs(db.getDatasource());
+			sqlNode.setDs(db.getDatasource());
 			sqlNode.setSql(sql);
 
 			return sqlNode;
@@ -149,60 +150,18 @@ public class App {
 		}
 	}
 
-	public App(String storageConfigFile) throws Exception {
-		dbCluster = new DbcpDBClusterImpl(EnumDB.MYSQL);
-		dbCluster.setShardInfoFromZk(true);
-		dbCluster.startup(storageConfigFile);
-
-		this.tables = dbCluster.getDBTableFromZk();
-	}
-
-	public void run() throws Exception {
-		boolean isRunning = true;
-
-		ConsoleReader creader = new ConsoleReader();
-		String cmd = null;
-		while (isRunning) {
-			cmd = creader.readLine(CMD_PROMPT);
-            if (cmd.endsWith(";")) {
-                cmd = cmd.substring(0, cmd.length() - 1);
-            }
-
-			try {
-				if (cmd.equals("exit")) {
-					isRunning = false;
-				} else if (cmd.toLowerCase().startsWith("select")) {
-					_handleSelect(cmd);
-				} else if (cmd.toLowerCase().startsWith("update")) {
-					_handleUpdate(cmd);
-				} else if (cmd.toLowerCase().startsWith("delete")) {
-					_handleDelete(cmd);
-				} else if (cmd.toLowerCase().equals("show")) {
-					_handleShow();
-				} else if (cmd.trim().equals("")) {
-				} else {
-					System.out.println("unknow command:\"" + cmd + "\", now support select, update, delete ");
-				}
-			} catch (CommandException e) {
-				System.out.println(e.getMessage());
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-		}
-	}
-
 	/**
 	 * handle select sql.
 	 */
 	private void _handleSelect(String cmd) throws SQLException {
-        SqlNode sqlNode = null;
-        if (cmd.indexOf(KEY_SHARDINGBY) > -1) {
-            sqlNode = parseShardingSqlNode(cmd);
-        } else {
-            sqlNode = parseGlobalSqlNode(cmd);
-        }
-		
-        String sql = sqlNode.getSql();
+		SqlNode sqlNode = null;
+		if (cmd.indexOf(KEY_SHARDINGBY) > -1) {
+			sqlNode = parseShardingSqlNode(cmd);
+		} else {
+			sqlNode = parseGlobalSqlNode(cmd);
+		}
+
+		String sql = sqlNode.getSql();
 
 		DataSource ds = sqlNode.getDs();
 		Connection conn = null;
@@ -215,13 +174,13 @@ public class App {
 
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int columnCount = rsmd.getColumnCount();
-            // show table header
-            StringBuilder header = new StringBuilder();
-            for (int i = 1; i <= columnCount; i++) {
-                header.append(rsmd.getColumnName(i)).append(" ");
-            }
-            System.out.println(header.toString());
-            // show table record
+			// show table header
+			StringBuilder header = new StringBuilder();
+			for (int i = 1; i <= columnCount; i++) {
+				header.append(rsmd.getColumnName(i)).append(" ");
+			}
+			System.out.println(header.toString());
+			// show table record
 			StringBuilder record = new StringBuilder();
 			while (rs.next()) {
 				for (int i = 1; i <= columnCount; i++) {
@@ -287,6 +246,49 @@ public class App {
 					table.getName(), type, table.getCluster(), shardingField, table.getShardingNum());
 		}
 	}
+	
+	public App(String storageConfigFile) throws Exception {
+		dbCluster = new DbcpDBClusterImpl(EnumDB.MYSQL);
+		dbCluster.setShardInfoFromZk(true);
+		dbCluster.startup(storageConfigFile);
+
+		this.tables = dbCluster.getDBTableFromZk();
+	}
+
+	public void run() throws Exception {
+		boolean isRunning = true;
+
+		ConsoleReader creader = new ConsoleReader();
+		creader.addCompletor(new JlineCompletor());
+		String cmd = null;
+		while (isRunning) {
+			cmd = creader.readLine(CMD_PROMPT);
+			if (cmd.endsWith(";")) {
+				cmd = cmd.substring(0, cmd.length() - 1);
+			}
+
+			try {
+				if (cmd.equals("exit")) {
+					isRunning = false;
+				} else if (cmd.toLowerCase().startsWith("select")) {
+					_handleSelect(cmd);
+				} else if (cmd.toLowerCase().startsWith("update")) {
+					_handleUpdate(cmd);
+				} else if (cmd.toLowerCase().startsWith("delete")) {
+					_handleDelete(cmd);
+				} else if (cmd.toLowerCase().equals("show")) {
+					_handleShow();
+				} else if (cmd.trim().equals("")) {
+				} else {
+					System.out.println("unknow command:\"" + cmd + "\", now support select, update, delete ");
+				}
+			} catch (CommandException e) {
+				System.out.println(e.getMessage());
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		if (args.length == 0) {
@@ -309,25 +311,51 @@ public class App {
 
 	}
 
-	private class SqlNode {
-        private DataSource ds;
-        private String sql;
+	private class JlineCompletor implements Completor {
 
-        public DataSource getDs() {
-            return ds;
-        }
-        
-        public void setDs(DataSource ds) {
-            this.ds = ds;
-        }
+		@Override
+		public int complete(String buffer, int cursor, List candidates) {
+            if (buffer.indexOf("from") > -1) {
+                String prefix = buffer.substring(buffer.indexOf("from") + 4).trim();
+                if (prefix.equals("")) {
+                    for (DBTable table : tables) {
+                        candidates.add(table.getName());
+                    }
+                } else {
+                    for (DBTable table : tables) {
+                        if (table.getName().startsWith(prefix)) {
+                            candidates.add(table.getName());
+                        }
+                    }
+                }
 
-        public String getSql() {
-            return sql;
-        }
-        
-        public void setSql(String sql) {
-            this.sql = sql;
-        }
+                cursor -= prefix.length();
+            }
+
+			return cursor;
+		}
+
 	}
-        
+
+	private class SqlNode {
+		private DataSource ds;
+		private String sql;
+
+		public DataSource getDs() {
+			return ds;
+		}
+
+		public void setDs(DataSource ds) {
+			this.ds = ds;
+		}
+
+		public String getSql() {
+			return sql;
+		}
+
+		public void setSql(String sql) {
+			this.sql = sql;
+		}
+	}
+
 }
